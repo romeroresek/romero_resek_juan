1ade55a8ce8cee8f5a3c92ef11fc88c56516a6c2
romeroresek <juanromero1@hotmail.com>
2022-11-28 10:46:40 -0300

    agregue archivo sql y mwb diagramas

--- modulo04/tp4/ejercicio4.sql
+++ modulo04/tp4/ejercicio4.sql
@@ -1,43 +1,106 @@
--- CREATE
-CREATE TABLE `silicon`.`alumno` (
-  `idalumno` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `dni` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idalumno`));
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: silicon
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
-CREATE TABLE `silicon`.`curso` (
-  `idcurso` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idcurso`));
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- RELACIONAR TABLA CON FK
-ALTER TABLE `silicon`.`curso` 
-ADD COLUMN `alumno` INT NULL AFTER `nombre`,
-ADD INDEX `alumno_idx` (`alumno` ASC) VISIBLE;
-;
-ALTER TABLE `silicon`.`curso` 
-ADD CONSTRAINT `alumno`
-  FOREIGN KEY (`alumno`)
-  REFERENCES `silicon`.`alumno` (`idalumno`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+--
+-- Table structure for table `alumno`
+--
 
--- Cargué tabla alumnos
-INSERT INTO `silicon`.`alumno` (`nombre`, `apellido`, `dni`) VALUES ('Juan', 'Perez', '22222222');
-INSERT INTO `silicon`.`alumno` (`nombre`, `apellido`, `dni`) VALUES ('Lucas', 'Rodriguez', '33333333');
-INSERT INTO `silicon`.`alumno` (`nombre`, `apellido`, `dni`) VALUES ('Andres', 'Ortega', '44444444');
+DROP TABLE IF EXISTS `alumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `dni` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `alumno`
+--
 
--- cargé tabla curso
-INSERT INTO `silicon`.`curso` (`nombre`) VALUES ('Introducción Programación');
-INSERT INTO `silicon`.`curso` (`nombre`) VALUES ('Programación Javascript');
-INSERT INTO `silicon`.`curso` (`nombre`) VALUES ('Curso React');
+LOCK TABLES `alumno` WRITE;
+/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
+INSERT INTO `alumno` VALUES (4,'Juan','Perez','22222222'),(5,'Lucas','Rodriguez','33333333'),(6,'Andres','Ortega','44444444');
+/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- cargé tabla curso alumno
-INSERT INTO `silicon`.`curso_has_alumno` (`curso_idcurso`, `alumno_idalumno`) VALUES ('3', '4');
-INSERT INTO `silicon`.`curso_has_alumno` (`curso_idcurso`, `alumno_idalumno`) VALUES ('4', '4');
-INSERT INTO `silicon`.`curso_has_alumno` (`curso_idcurso`, `alumno_idalumno`) VALUES ('5', '5');
-INSERT INTO `silicon`.`curso_has_alumno` (`curso_idcurso`, `alumno_idalumno`) VALUES ('5', '6');
+--
+-- Table structure for table `alumno_curso`
+--
 
+DROP TABLE IF EXISTS `alumno_curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumno_curso` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_curso` int NOT NULL,
+  `id_alumno` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_curso_has_alumno_alumno1_idx` (`id_alumno`),
+  KEY `fk_curso_has_alumno_curso_idx` (`id_curso`),
+  CONSTRAINT `fk_curso_has_alumno_alumno1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id`),
+  CONSTRAINT `fk_curso_has_alumno_curso` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `alumno_curso`
+--
+
+LOCK TABLES `alumno_curso` WRITE;
+/*!40000 ALTER TABLE `alumno_curso` DISABLE KEYS */;
+INSERT INTO `alumno_curso` VALUES (1,3,4),(2,4,4),(3,5,5),(4,5,6);
+/*!40000 ALTER TABLE `alumno_curso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (3,'Introducción Programación'),(4,'Programación Javascript'),(5,'Curso React');
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-28 10:44:31
